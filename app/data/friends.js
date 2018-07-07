f63bca98d91379a86b17632db8c892255f40aaf0
peterpeterhaha <36021063+peterpeterhaha@users.noreply.github.com>
2018-07-07 05:53:34 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+let friends = [
+{
+  "name":"Junkrat",
+  "photo":"https://vignette.wikia.nocookie.net/overwatch/images/5/53/Junkrat_portrait.png/revision/latest/scale-to-width-down/342?cb=20160429040823",
+  "scores":[
+      5,
+      4,
+      1,
+      5,
+      1
+    ] 
+},
+{
+  "name":"Widow Maker",
+  "photo":"https://vignette.wikia.nocookie.net/overwatch/images/1/16/Widowmaker_portrait.png/revision/latest/scale-to-width-down/350?cb=20160429041501",
+  "scores":[
+      1,
+      5,
+      5,
+      5,
+      3
+    ]
+},
+{
+  "name":"Winston",
+  "photo":"https://i.ytimg.com/vi/qJcKQd7HFJM/maxresdefault.jpg",
+  "scores":[
+      1,
+      3,
+      3,
+      1,
+      5
+    ]
+},
+{
+  "name":"Zenyatta",
+  "photo":"https://vignette.wikia.nocookie.net/overwatch/images/f/f5/Zenyatta_portrait.png/revision/latest/scale-to-width-down/350?cb=20160429042336",
+  "scores":[
+      1,
+      4,
+      3,
+      1,
+      5
+    ]
+},
+{
+  "name":"Hammond",
+  "photo":"https://media0dk-a.akamaihd.net/79/20/ae90136dbed3417d427b26551b96671b.jpeg",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+},
+{
+  "name":"Brigette",
+  "photo":"https://d1u5p3l4wpay3k.cloudfront.net/overwatch_gamepedia/thumb/8/8a/Brigitte_Concept.png/255px-Brigitte_Concept.png?version=46c36e2c1e9ba16db5697729a25b676c",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+}
+];
+
+module.exports = friends;
+
+
